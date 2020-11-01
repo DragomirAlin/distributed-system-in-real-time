6c004bc9a9e613348eeae25d2ebea5ae556961eb
DragomirAlin <dragomirdanielalin@Yahoo.com>
2020-11-01 18:21:13 +0200

    added reconnect funcion

--- esp8266/mqtt-esp.ino
+++ esp8266/mqtt-esp.ino
@@ -13,8 +13,46 @@ void setup() {
  
   Serial.begin(115200);
  
-  WiFi.begin(ssid, password);
+  connect();
  
+  client.publish("esp/test", "hello"); //Topic name
+  client.subscribe("esp/test");
+  client.subscribe("esp/room");
+//  while(true){
+//     client.publish("esp/test", "hello"); //Topic name
+//     client.publish("temperature", "22.3");
+//     client.publish("umid", "21");
+//     Serial.println(client.subscribe("light"));
+//     delay(1000);
+//  }
+ 
+}
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message = "";
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    message += (char)payload[i];
+  }
+  Serial.println();
+}
+
+ 
+void loop() {
+  if (!client.connected()) {
+    connect();
+  }
+  client.loop();
+}
+
+
+void connect(){
+  WiFi.begin(ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
@@ -26,49 +64,13 @@ void setup() {
  
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
- 
     if (client.connect("ESP8266Client")) {
- 
       Serial.println("connected");  
- 
     } else {
- 
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
- 
     }
   }
- 
-  client.publish("esp/test", "hello"); //Topic name
-  String message = client.subscribe("esp/test");
-  Serial.print(message);
-  
-  while(true){
-     client.publish("esp/test", "hello"); //Topic name
-     client.publish("temperature", "22.3");
-     client.publish("umid", "21");
-     Serial.println(client.subscribe("light"));
-     delay(1000);
-  }
- 
 }
- 
-void callback(char* topic, byte* payload, unsigned int length) {
- 
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
- 
-  Serial.print("Message:");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
- 
-  Serial.println();
-  Serial.println("-----------------------");
- 
-}
- 
-void loop() {
-  client.loop();
-}
+
