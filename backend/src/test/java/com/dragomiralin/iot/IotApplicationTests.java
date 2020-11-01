90d27e9b1a585c08e348db75d5ba984d2e847f91
DragomirAlin <dragomirdanielalin@Yahoo.com>
2020-11-01 18:24:21 +0200

    created spring project with initializr

---
+++ backend/src/test/java/com/dragomiralin/iot/IotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragomiralin.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
