0c25411b7887400d4e931b6e4db69231aad3299d
DragomirAlin <dragomirdanielalin@Yahoo.com>
2020-11-09 17:52:34 +0200

    added publish service

--- backend/src/main/java/com/dragomiralin/iot/mqtt/publish/PublishService.java
+++ backend/src/main/java/com/dragomiralin/iot/mqtt/publish/PublishService.java
@@ -1,4 +1,21 @@
 package com.dragomiralin.iot.mqtt.publish;
 
+import com.dragomiralin.iot.mqtt.config.MqttConfig;
+import com.dragomiralin.iot.mqtt.model.Publish;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class PublishService {
+
+    public void publishMessage(Publish publishObject) throws MqttException {
+        MqttMessage mqttMessage = new MqttMessage(publishObject.getMessage().getBytes());
+        mqttMessage.setQos(publishObject.getQos());
+        mqttMessage.setRetained(publishObject.getRetained());
+
+        MqttConfig.getInstance().publish(publishObject.getTopic(), mqttMessage);
+    }
 }
