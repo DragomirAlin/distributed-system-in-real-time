d6e6f2dd3a1200a89bb806305ddcb32419a144a4
Dragomir Alin <alin.dragomir@cloudify.ro>
2020-12-26 17:23:55 +0200

    added subscribe/unsubscribe

---
+++ frontend/src/app/mqtt/mqtt.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MqttService } from '../api';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-mqtt',
+  templateUrl: './mqtt.component.html',
+  styleUrls: ['./mqtt.component.css']
+})
+export class MqttComponent implements OnInit {
+
+  constructor(private mqttService: MqttService) { }
+
+  ngOnInit(): void {
+  }
+
+  subscribeTopic(data: { topic: string; }){
+    this.mqttService.subscribeChannel(data.topic).subscribe();
+  }
+
+  unsubscribeTopic(unsub: { utopic: string; }){
+    this.mqttService.unsubscribeChannel(unsub.utopic).subscribe();
+  }
+}
