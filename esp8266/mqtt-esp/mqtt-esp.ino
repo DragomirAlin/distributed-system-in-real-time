957976c72b00ce09942225c34b5f5d88486b03cc
Alin <dragomirdanielalin@yahoo.com>
2021-01-10 14:36:09 +0200

    added RTOS to esp

--- esp8266/mqtt-esp/mqtt-esp.ino
+++ esp8266/mqtt-esp/mqtt-esp.ino
@@ -3,12 +3,15 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <DHT.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
 #define DHTPIN D3
 #define DHTTYPE DHT11
  
-const char* ssid = ""; // Enter your WiFi name
-const char* password =  ""; // Enter WiFi password
-const char* mqttServer = "192.168.1.5";
+const char* ssid = "DIGI_9c8588"; // Enter your WiFi name
+const char* password =  "1769140d"; // Enter WiFi password
+const char* mqttServer = "192.168.1.4";
 const int mqttPort = 1883;
 String message = "";
 
@@ -16,74 +19,91 @@ int senseMode = 0;
 
 size_t measureJsonPretty(const JsonDocument& doc); 
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-DHT dht3 = DHT(D1, DHTTYPE);
+WiFiClient wifiClient;
+PubSubClient MQTTclient(wifiClient);
+DHT dht3 = DHT(DHTPIN, DHTTYPE);
+SemaphoreHandle_t sema_MQTT_KeepAlive;
 
 void setup() {
+  
+  sema_MQTT_KeepAlive   = xSemaphoreCreateBinary();
+  xSemaphoreGive( sema_MQTT_KeepAlive ); // found keep alive can mess with a publish, stop keep alive during publish
+
   Serial.begin(115200);
-  dht3.begin();
-  connect();
- 
-  client.subscribe("command");
+  dht3.begin(); 
+  xTaskCreatePinnedToCore( MQTTkeepalive, "MQTTkeepalive", 20000, NULL, 3, NULL, 1 );
+  xTaskCreatePinnedToCore( Acquisition, "Acquisition", 20000, NULL, 4, NULL, 1);
 }
  
-void callback(char* topic, byte* payload, unsigned int length) {
-  String message = "";
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
- 
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-    message += (char)payload[i];
-  }
-
-}
-
- 
-void loop() {
+void Acquisition( void *pvParameters )
+{
+  float temperature = 0.0f;
+  float humidity    = 0.0f;
   
-  if (!client.connected()) {
-    connect();
+  while ( !MQTTclient.connected() )
+  {
+    vTaskDelay( 250 );
   }
+  for (;;)
+  {
+    StaticJsonDocument<300> doc;
+    doc["moduleID"] = 1;
+    doc["temperature"] = (float)dht3.readTemperature();
+    doc["humidity"] = (float)dht3.readHumidity();
+    char buffer[256];
+    serializeJson(doc, buffer);
+    xSemaphoreTake( sema_MQTT_KeepAlive, portMAX_DELAY );
+    if ( client.connected() )
+    {
+      client.publish("esp1", buffer);
 
-
-  StaticJsonDocument<300> doc;
-  doc["moduleID"] = 1;
-  doc["temperature"] = (float)dht3.readTemperature();
-  doc["humidity"] = (float)dht3.readHumidity();
-
-  char buffer[256];
-  serializeJson(doc, buffer);
-
-  client.publish("esp1", buffer);
-  delay(1000);
-
-  
-  client.loop();
-}
-
-
-void connect(){
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-  }
-  Serial.println("Connected to the WiFi network");
- 
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(callback);
- 
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (client.connect("ESP8266Client")) {
-      Serial.println("connected");  
-    } else {
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
     }
+    xSemaphoreGive( sema_MQTT_KeepAlive );
+    vTaskDelay( 1000 * 15 );
+  }
+  vTaskDelete ( NULL );
+}
+
+*/
+void MQTTkeepalive( void *pvParameters )
+{
+  MQTTclient.setKeepAlive( 90 ); 
+  for (;;)
+  {
+    if ((wifiClient.connected()) && (WiFi.status() == WL_CONNECTED))
+    {
+      xSemaphoreTake( sema_MQTT_KeepAlive, portMAX_DELAY );
+      MQTTclient.loop();
+      xSemaphoreGive( sema_MQTT_KeepAlive );
+    }
+    else {
+      if ( !(WiFi.status() == WL_CONNECTED) )
+      {
+        connectToWiFi();
+      }
+      connectToMQTT();
+    }
+    vTaskDelay( 250 ); 
+  }
+  vTaskDelete ( NULL );
+}
+
+void connectToMQTT()
+{
+  while ( !MQTTclient.connected() )
+  {
+    MQTTclient.setServer(mqttServer, mqttPort);
+    vTaskDelay( 250 );
   }
 }
+
+void connectToWiFi()
+{
+  while ( WiFi.status() != WL_CONNECTED )
+  {
+    WiFi.begin(ssid, password);
+    vTaskDelay( 4000 );
+  }
+}
+
+void loop() { }
